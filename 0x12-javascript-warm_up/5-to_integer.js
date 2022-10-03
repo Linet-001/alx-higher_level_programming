dd83df12918c46a01d6d265258a97aff557f48ba
Linet-001 <linetmuchunu@gmail.com>
2022-10-03 23:25:48 +0300

    upload

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-/* script that prints My number: <first argument converted in integer>
-if the first argument can be converted to an integer */
-const argv = parseInt(process.argv[2]);
-if (Number.isNaN(argv)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + argv);
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
