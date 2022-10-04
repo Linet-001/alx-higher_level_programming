549ed96f5e851331d1fffe942b6eefa175160fb6
Linet-001 <linetmuchunu@gmail.com>
2022-10-04 21:51:15 +0300

    my task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// JS Script
+module.exports = class Rectangle {
+  constructor (w, h ) {
+    if (w > 0 && h > 0) {
+     this.width = w;
+     this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = o; i < this.height; i++0 {
+     console.log('X'.repeat(this.width));
+    }
+  }
+  
+  rotate () {
+    this.width = [this.height, this.height = this.width][0];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+   }
+};
+   
